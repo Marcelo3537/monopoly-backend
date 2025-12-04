64655577b037e41b49dd9136f8d65beb5ded103b
Marcelo <marcelooscar250@gmail.com>
2025-12-04 17:47:24 +0100

    Clase Historial_recompensa

--- BACKEND/Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ BACKEND/Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74382f07c7ceeb65e853ea2edda69b377d6f5252")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c144e4af9b9de783bd3db4b7f8ca55a2014ede86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
