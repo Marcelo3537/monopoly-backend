c144e4af9b9de783bd3db4b7f8ca55a2014ede86
Marcelo <marcelooscar250@gmail.com>
2025-12-04 17:46:35 +0100

    Clase Logro

---
+++ BACKEND/Backend/Backend/Models/Logro.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
+{
+    public class Producto
+    {
+        [Key] // Esta es la Clave Primaria
+        public int Id { get; set; }
+    }
+}
--- BACKEND/Backend/Backend/obj/Release/net8.0/Backend.AssemblyInfo.cs
+++ BACKEND/Backend/Backend/obj/Release/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cabe1643c7f653a9ed93267b480703deb3d77f6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74382f07c7ceeb65e853ea2edda69b377d6f5252")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
