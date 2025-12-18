bde424c0feb111368b462472d423853ac1846f63
Marcelo <marcelooscar250@gmail.com>
2025-12-18 18:01:36 +0100

    Data Anotations

--- Backend/Backend/Model/Usuario.cs
+++ Backend/Backend/Model/Usuario.cs
@@ -5,9 +5,16 @@ namespace Backend.Models
 {
     public class Usuario
     {
+        [Key]
         public int IdUsuario { get; set; }
 
+        [Required]
+        [MaxLength(50)]
         public string UsuarioNombre { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [EmailAddress]
         public string Correo { get; set; }
         public string ContrasenyaHash { get; set; }
 
@@ -18,10 +25,14 @@ namespace Backend.Models
         public int Gemas { get; set; }
 
         public int Nivel { get; set; }
+
+        [Range(0, 100)]
         public decimal ProgresoPorcentaje { get; set; }
 
         public int TotalPartidas { get; set; }
         public int TotalVictorias { get; set; }
+
+        [Range(0, 1)]
         public decimal RatioVictoria { get; set; }
 
         public int EloRating { get; set; }
--- Backend/Backend/Model/UsuarioPartida.cs
+++ Backend/Backend/Model/UsuarioPartida.cs
@@ -1,12 +1,18 @@
-﻿namespace Backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
 {
     public class UsuarioPartida
     {
+        [Key]
         public int IdUsuarioPartida { get; set; }
 
+        [ForeignKey("Usuario")]
         public int IdUsuario { get; set; }
         public Usuario Usuario { get; set; }
 
+        [ForeignKey("Partida")]
         public int IdPartida { get; set; }
         public Partida Partida { get; set; }
 
